d5a7120f5f26afb779b31535b544acfa26b842a0
Viktor <viktopiniaginya@gmail.com>
2023-03-10 15:34:26 +0300

    Initial Commit

---
+++ SShop/SShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SShopApp.swift
+//  SShop
+//
+//  Created by Виктор on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
