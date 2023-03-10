e00c340e112232f276a3f79ea26ab6983d5eddad
Viktor <viktopiniaginya@gmail.com>
2023-03-10 17:40:51 +0300

    BigButtonStyle set

---
+++ SShop/CustomViews/Configurations/BigButtonStyle.swift
@@ -0,0 +1,18 @@
+//
+//  MainBigButton.swift
+//  SShop
+//
+//  Created by Виктор on 10.03.2023.
+//
+
+import SwiftUI
+
+struct BigButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.mainBigButtonPurple)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+    }
+}
--- SShop/Extension/Color+Extension.swift
+++ SShop/Extension/Color+Extension.swift
@@ -10,4 +10,5 @@ import SwiftUI
 extension Color {
     static let projectTextFieldGray = Color(red: 0.91, green: 0.91, blue: 0.91)
     static let projectTextGray = Color(red: 0.482, green: 0.482, blue: 0.482)
+    static let mainBigButtonPurple = Color(red: 0.306, green: 0.333, blue: 0.843)
 }
