1831946c3e3424cdd047e1d37e95cecac9a1ba9e
Viktor <viktopiniaginya@gmail.com>
2023-03-10 17:55:57 +0300

    Made TextField stack

---
+++ SShop/Sources/Scenes/SignInScene/Model/InputProfileModel.swift
@@ -0,0 +1,14 @@
+//
+//  InputProfileModel.swift
+//  SShop
+//
+//  Created by Виктор on 10.03.2023.
+//
+
+import Foundation
+
+struct InputProfileModel {
+    let firstName: String?
+    let secondName: String?
+    let email: String?
+}
---
+++ SShop/Sources/Scenes/SignInScene/View/ViewParts/TextFieldsStack.swift
@@ -0,0 +1,30 @@
+//
+//  TextFieldsStack.swift
+//  SShop
+//
+//  Created by Виктор on 10.03.2023.
+//
+
+import SwiftUI
+
+struct TextFieldsStack: View {
+    var spacing: CGFloat
+    var body: some View {
+        var textFieldNames = [
+            "First name",
+            "Second name",
+            "Email"
+        ]
+        VStack(spacing: spacing) {
+            ForEach(textFieldNames, id: \.self) { item in
+                MainTextField(inputValue: "", placeholderText: item)
+            }
+        }
+    }
+}
+
+struct TextFieldsStack_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldsStack(spacing: 35)
+    }
+}
